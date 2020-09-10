400949e095f9ebee17415535d2754d7b1b7a21ac
lucaspires-source <60105171+lucaspires-source@users.noreply.github.com>
2020-09-10 11:42:53 -0300

    the app is running

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,32 +1,41 @@
 import React, { Component } from "react";
 import "./estilo.css";
 class FormularioCadastro extends Component {
-  constructor(){
-    super();
+  constructor(props){
+    super(props);
     this.titulo=""
     this.texto=""
   }
   
-  handleMudançaTitulo(event){
+  _handleMudançaTitulo(event){
+    event.stopPropagation()
     this.titulo= event.target.value;
   }
-  handleMudançaTexto(event){
+  
+  _handleMudançaTexto(event){
+    event.stopPropagation()
     this.texto=event.target.value;  
+  }
+
+  _criarNota(event){
+    event.preventDefault()
+    event.stopPropagation()
+    this.props.criarNota(this.titulo,this.texto)
   }
   render() {
     return (
-      <form className="form-cadastro ">
+      <form className="form-cadastro " onSubmit={this._criarNota.bind(this)}>
         <input
           type="text"
           placeholder="Título"
           className="form-cadastro_input"
-          onChange={this.handleMudançaTitulo.bind(this)}
+          onChange={this._handleMudançaTitulo.bind(this)}
         />
         <textarea
           rows={15}
           placeholder="Escreva sua nota..."
           className="form-cadastro_input"
-          onChange={this.handleMudançaTexto.bind(this)}
+          onChange={this._handleMudançaTexto.bind(this)}
         />
         <button className="form-cadastro_input form-cadastro_submit">
           Criar Nota
