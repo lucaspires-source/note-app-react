b89b4e78e7b98ca3eb7dfd1e8351a4af1e0ad6ec
lucaspires-source <60105171+lucaspires-source@users.noreply.github.com>
2020-09-10 09:19:32 -0300

    Event added

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,6 +1,18 @@
 import React, { Component } from "react";
 import "./estilo.css";
 class FormularioCadastro extends Component {
+  constructor(){
+    super();
+    this.titulo=""
+    this.texto=""
+  }
+  
+  handleMudançaTitulo(event){
+    this.titulo= event.target.value;
+  }
+  handleMudançaTexto(event){
+    this.texto=event.target.value;  
+  }
   render() {
     return (
       <form className="form-cadastro ">
@@ -8,11 +20,13 @@ class FormularioCadastro extends Component {
           type="text"
           placeholder="Título"
           className="form-cadastro_input"
+          onChange={this.handleMudançaTitulo.bind(this)}
         />
         <textarea
           rows={15}
           placeholder="Escreva sua nota..."
           className="form-cadastro_input"
+          onChange={this.handleMudançaTexto.bind(this)}
         />
         <button className="form-cadastro_input form-cadastro_submit">
           Criar Nota
